9c84debd3edb5fac225924aca35cbf5fe3354f3e
GchanYT <56825628+kitecin@users.noreply.github.com>
2023-10-01 01:33:44 +0900

    feat: ✨ Drawer navigator
    Drawer navigator was added, tested. Gestures are not working.

--- App.tsx
+++ App.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 import {Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {StackNavigator} from './src/navigator/StackNavigator';
+import {DrawerNavigator} from './src/navigator/DrawerNavigator';
+import 'react-native-gesture-handler';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <DrawerNavigator />
+      {/* <StackNavigator /> */}
     </NavigationContainer>
   );
 };
